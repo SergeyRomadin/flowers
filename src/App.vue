80b14c62aa50e80556e18f7e8e47334a0915831a
Sergey Romadin <voidcode.music@gmail.com>
2022-09-05 23:20:36 +0300

    Передал пропсы в img

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<header><p>Header</p></header>
+<ProductItems/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import ProductItems from './components/ProductItems.vue'
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  export default {
+    components : {
+    ProductItems
   }
-}
+  }
+</script>
+
+<style lang="scss">
+
 </style>
+
+
---
+++ src/components/ProductItems.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <ul class="product-items">
+    <li>
+      <ProductItem img="https://static.tildacdn.com/tild3336-3261-4339-b162-303032353733/florentin-misty-bubb.jpg" />
+    </li>
+    <li>
+      <ProductItem img="https://static.tildacdn.com/tild3336-3261-4339-b162-303032353733/florentin-misty-bubb.jpg" />
+    </li>
+    <li>
+      <ProductItem img="https://static.tildacdn.com/tild3336-3261-4339-b162-303032353733/florentin-misty-bubb.jpg" />
+    </li>
+  </ul>
+
+</template>
+
+<script>
+  import ProductItem from './ProductItem.vue'
+
+  export default {
+    components : {
+    ProductItem,
+}
+  }
+</script>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+}
+
+.product-items {
+  display: flex;
+}
+</style>
